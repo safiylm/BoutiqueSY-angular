7388867e503534e44923321c4486eaf9f6aa78fc
safiylm <121391404+safiylm@users.noreply.github.com>
2024-10-05 16:51:44 +0200

    new component for product page

---
+++ src/app/main-infos-produit/main-infos-produit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-infos-produit',
+  templateUrl: './main-infos-produit.component.html',
+  styleUrls: ['./main-infos-produit.component.scss']
+})
+export class MainInfosProduitComponent {
+
+}
---
+++ src/app/photos-viewer/photos-viewer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-photos-viewer',
+  templateUrl: './photos-viewer.component.html',
+  styleUrls: ['./photos-viewer.component.scss']
+})
+export class PhotosViewerComponent {
+
+}
