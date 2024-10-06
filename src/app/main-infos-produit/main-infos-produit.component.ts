4c65e9891db6c3a04bee9bdbfc4d40ccd6ccdbfe
safiylm <121391404+safiylm@users.noreply.github.com>
2024-10-06 22:08:55 +0200

    informations du produit

--- src/app/main-infos-produit/main-infos-produit.component.ts
+++ src/app/main-infos-produit/main-infos-produit.component.ts
@@ -9,8 +9,11 @@ export class MainInfosProduitComponent {
 
   title: string ="Puffy Charlotte Ring";
   price: number = 68;
-  colors : string[]= ['Gold Vermeil, "Sterling Silver']
+  color1 : object= { 'nom':'Sterling Silver', 'code': "#c8c8c8"}
+  color2 : object= {'nom':'Gold Vermeil','code': '#eac37c'}
   size : number[]=[4,5,6,7,8,9,10,11,12]
-
+  details:string ="- Top Width: 12.5 mm - Shank Thickness: 2 mm"
+  description :string = "Designed for doing big things. The boldest pieces in our Charlotte Collection want you to take up space—all of it. "
+  materials :string ="925 Sterling Silver is a lightweight metal made of 92.5% pure silver. It’s highly durable and designed for everyday wear."
 
 }
