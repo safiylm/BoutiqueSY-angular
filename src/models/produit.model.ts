6c9a2fbaa8a90da6498cb0e49430c06208d332b4
safiylm <121391404+safiylm@users.noreply.github.com>
2024-10-09 22:34:39 +0200

    Create produit.model.ts

---
+++ src/models/produit.model.ts
@@ -0,0 +1,13 @@
+export class Produit {
+    constructor(
+        public _id : string ,
+        public title: string,
+        public color: string[],
+        public image: string[],
+        public prix : number,
+        public size : string[],
+        public details: string,
+        public description: string,
+        public materials: string
+    ){}
+}
