207d9113884885cf129aa5562de39eaaa540b8c3
safiylm <121391404+safiylm@users.noreply.github.com>
2024-10-15 16:30:44 +0200

    update nav

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
+
 export class NavComponent {
+
+  constructor(private route: ActivatedRoute) { }
+  categorie !: string;
+  @ViewChild("aerarring") aerarring !: ElementRef;
+  @ViewChild("arings") arings !: ElementRef;
+  @ViewChild("anecklaces") anecklaces !: ElementRef;
+  @ViewChild("abracelets") abracelets !: ElementRef;
+
+  ngOnInit() {
+    this.categorie = this.route.snapshot.paramMap.get('categorie')!;
+  }
+
+  ngAfterViewInit() {
+    if (this.categorie == "Earrings") this.aerarring.nativeElement.style.textDecoration = "underline";
+    if (this.categorie == "Rings") this.arings.nativeElement.style.textDecoration = "underline";
+    if (this.categorie == "Necklaces") this.anecklaces.nativeElement.style.textDecoration = "underline";
+    if (this.categorie == "Bracelets") this.abracelets.nativeElement.style.textDecoration = "underline";
+  }
 
 }
