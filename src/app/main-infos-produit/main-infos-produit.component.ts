0619ffb5ab9cf49f7f34829b261c663e3668cb8a
safiylm <121391404+safiylm@users.noreply.github.com>
2024-10-16 09:40:16 +0200

    add produitService

--- src/app/main-infos-produit/main-infos-produit.component.ts
+++ src/app/main-infos-produit/main-infos-produit.component.ts
@@ -1,28 +1,32 @@
 import { Component, AfterViewInit, ViewChild, ElementRef, Input } from "@angular/core";
 import { Produit } from "src/models/produit.model";
+import { ProduitService } from "src/services/produit-service";
 
 @Component({
   selector: 'app-main-infos-produit',
   templateUrl: './main-infos-produit.component.html',
   styleUrls: ['./main-infos-produit.component.scss']
 })
-export class MainInfosProduitComponent   implements AfterViewInit {
+export class MainInfosProduitComponent implements AfterViewInit {
 
-  color1 = { 'nom':'Sterling Silver', 'code': "#c8c8c8"}
-  color2 = {'nom':'Gold Vermeil','code': '#eac37c'}
+  color1 = { 'nom': 'Sterling Silver', 'code': "#c8c8c8" }
+  color2 = { 'nom': 'Gold Vermeil', 'code': '#eac37c' }
 
-   @Input() produit !: Produit;
+  @Input() produit !: Produit;
+  @ViewChild("btncolor") btncolor !: ElementRef;
+  @ViewChild("btncolor2") btncolor2 !: ElementRef;
 
-    @ViewChild("btncolor") btncolor !: ElementRef;
-    @ViewChild("btncolor2") btncolor2 !: ElementRef;
-  
-    ngAfterViewInit() {
-      // Après l'initialisation de la vue, nous pouvons accéder en toute
-      // sécurité à notre élément référencé.
-      this.btncolor.nativeElement.style.backgroundColor = this.color1.code
-      this.btncolor2.nativeElement.style.backgroundColor = this.color2.code
-      ;
-    }
-  
-   
+  constructor(protected produitService : ProduitService){
+
   }
+
+  ngAfterViewInit() {
+    // Après l'initialisation de la vue, nous pouvons accéder en toute
+    // sécurité à notre élément référencé.
+    this.btncolor.nativeElement.style.backgroundColor = this.color1.code
+    this.btncolor2.nativeElement.style.backgroundColor = this.color2.code
+      ;
+  }
+
+
+}
