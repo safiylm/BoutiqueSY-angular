ccf93dec24a03da51fafbad504bcc2cb56c98093
safiylm <121391404+safiylm@users.noreply.github.com>
2024-10-16 08:57:22 +0200

    update login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +8,31 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  dejaUnCompte: boolean = true;
+
+  LoginForm = new FormGroup({
+    email: new FormControl("", [Validators.required, Validators.email]),
+    password: new FormControl("", Validators.required),
+  });
+
+  SignInForm = new FormGroup({
+    nom: new FormControl("", Validators.required),
+    prenom: new FormControl("", Validators.required),
+    email: new FormControl("", [Validators.required, Validators.email]),
+    password: new FormControl("", Validators.required),
+  });
+
+  toggleLogin() {
+    this.dejaUnCompte = !this.dejaUnCompte;
+  }
+
+  submitLogin(){
+    console.log(this.LoginForm.value['email'])
+    console.log(this.LoginForm.value['password'])
+  }
+
+  submitSignIn(){
+    console.log(this.SignInForm)
+
+  }
 }
