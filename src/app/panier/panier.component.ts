9759ad420f8517c975ec9a9cba5ca0635ca742f7
safiylm <121391404+safiylm@users.noreply.github.com>
2024-10-16 09:39:56 +0200

    update panier

--- src/app/panier/panier.component.ts
+++ src/app/panier/panier.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Produit } from 'src/models/produit.model';
+import { ProduitService } from 'src/services/produit-service';
 
 @Component({
   selector: 'app-panier',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./panier.component.scss']
 })
 export class PanierComponent {
+  products !: Produit[];
 
+  constructor(protected produitService : ProduitService){}
+
+  ngOnInit(){
+    this.products = this.produitService.getPanierList();
+  } 
 }
