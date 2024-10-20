b64477ca303f6dc718ea8f686ee5e34b9d845a59
safiylm <121391404+safiylm@users.noreply.github.com>
2024-10-20 20:56:00 +0200

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { LoginComponent } from './login/login.component';
 import { PanierComponent } from './panier/panier.component';
 import { WishlistComponent } from './wishlist/wishlist.component';
 import { SearchComponent } from './search/search.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -38,6 +39,7 @@ import { SearchComponent } from './search/search.component';
     SearchComponent
   ],
   imports: [
+   HttpClientModule, 
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule
