fb530a867cdca79809a05b8abd01c54df5216b0d
safiylm <121391404+safiylm@users.noreply.github.com>
2024-10-20 20:56:44 +0200

    Update produit.model.ts

--- src/models/produit.model.ts
+++ src/models/produit.model.ts
@@ -1,14 +1,14 @@
 export class Produit {
     constructor(
         public _id : string ,
-        public title: string,
+        public name: string,
         public categorie: string,
         public color: string[],
         public image: string[],
         public prix : number,
         public size : string[],
-        public details: string,
+        public detail: string,
         public description: string,
-        public materials: string
+        public material: string
     ){}
 }
