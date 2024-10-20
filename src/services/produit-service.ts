0dedd41bea319f4d1323bb3fd52b07a335a74fff
safiylm <121391404+safiylm@users.noreply.github.com>
2024-10-20 20:56:38 +0200

    get product from backend (db)

--- src/services/produit-service.ts
+++ src/services/produit-service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Produit } from '../models/produit.model';
+import { Router } from '@angular/router';
+import {
+    HttpClient,
+} from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { PRECONNECT_CHECK_BLOCKLIST } from '@angular/common';
 
 @Injectable({
     providedIn: 'root'
@@ -10,50 +13,7 @@ import { PRECONNECT_CHECK_BLOCKLIST } from '@angular/common';
 export class ProduitService {
 
     liste_produits: Produit[] = [
-        new Produit(
-            "0",
-            "Bold Huggie Hoops",
-            "Earrings",
-            ['14k Yellow Gold'],
-            ["https://cdn.shopify.com/s/files/1/0837/7489/8461/files/0-BoldHuggieHoops-14K-Angled_019_dd57d6ce-3452-4d5a-acb9-c949dd851e26.jpg",
-                "https://cdn.shopify.com/s/files/1/0837/7489/8461/files/1-BoldHuggieHoops-14K-Stack1_031_39ce9bd2-f10f-4f9a-be17-9b612257d8a2.jpg",
-                "https://cdn.shopify.com/s/files/1/0837/7489/8461/files/2-BoldHuggieHoops-14K-Stack2_026_b2f59923-f2c5-476c-bac9-52d5c060571d.jpg",
-                "https://cdn.shopify.com/s/files/1/0837/7489/8461/files/3-BoldHuggieHoops-14K-Clasp_115_fef2a0ec-70ca-4818-a515-02d56da1d0b3.jpg",
-                "https://cdn.shopify.com/s/files/1/0837/7489/8461/files/4_BoldHuggieHoops_yg_alt2_6f831baf-9a9e-4f18-8e90-12186e47c7d8.jpg"
-            ],
-            168,
-            [""], "- Outer Diameter: 12 mm - Width: 3.2 mm",
-            "Formerly known as the Bold Hoops. You know them, you love them: they're your favorite hoops. Those outfit-elevating, mood-boosting, can't-go-wrong hoops. ",
-            "14k Gold When properly cared for, our 14k gold pieces can last forever. With its beautiful, subtle hue, you can wear your jewelry every day, everywhere."
 
-        ),
-        new Produit("1", "Tube Huggie Hoops", "Earrings",
-            ['Gold Vermeil'],
-            ["https://cdn.shopify.com/s/files/1/0837/7489/8461/files/0-NewHoopStandard_TubeHoopsHuggie12mm_V_ANGLED_175_2c53943e-96fd-4b73-89b0-c1a91be0ced8.jpg",
-                "https://cdn.shopify.com/s/files/1/0837/7489/8461/files/1-NewHoopStandard_TubeHoopsHuggie12mm_V_Solo_021_afba5c6f-8ff7-4e59-a345-943c6223ea2b.jpg",
-                "https://cdn.shopify.com/s/files/1/0837/7489/8461/files/3-NewHoopStandard_TubeHoopsHuggie12mm_V_FLATLAY_226_ca50f967-1f10-4545-85ad-32a8241494dc.jpg",
-                "https://cdn.shopify.com/s/files/1/0837/7489/8461/files/2_Tube-Hoops_V_Annotations_0f1c1c54-1d10-40dc-ad03-f8b98d1d9324.jpg",
-                "https://cdn.shopify.com/s/files/1/0837/7489/8461/files/4-NewHoopStandard_TubeHoopsHuggie12mm_V_CLASP_308_86615319-b9f9-47ce-927c-ccf65ce43f28.jpg",
-            ],
-            48, [""],
-            "- Outer Diameter: 12 mm - Width: 2 mm - Thickness: 2 mm",
-            "A modern take on tubes. These bold hoops are lightweight enough to wear effortlessly, featuring a clicker closure for maximum comfort and security.",
-            "Not to be confused with regular gold plating, our vermeil is a thick layer of 18k solid gold on sterling silver meaning it will last longer. You get the look & feel of gold jewelry at a fraction of the price."),
-
-        new Produit("2", "Pavé Diamond Pearl Huggies", "Earrings",
-            ['14k Yellow Gold'],
-            ["https://cdn.shopify.com/s/files/1/0837/7489/8461/files/0_PaveDiamondPearlHuggies_Earrings_YG_Hero_c1e9f2b9-1182-4070-a067-09e6ed47bb88.jpg",
-                "https://cdn.shopify.com/s/files/1/0837/7489/8461/files/2_PaveDiamondPearlHuggies_Earrings_YG_Solo_Rich.jpg",
-                "https://cdn.shopify.com/s/files/1/0837/7489/8461/files/3_PaveDiamondPearlHuggies_Earrings_YG_Stacked_Fair_f9b40bab-5881-47a7-a71f-ce27ea55ed43.jpg",
-                "https://cdn.shopify.com/s/files/1/0837/7489/8461/files/4_PaveDiamondPearlHuggies_Earrings_YG_FlayLay_9d570915-4c9a-4803-839f-4563a2b63b86.jpg",
-                "https://cdn.shopify.com/s/files/1/0837/7489/8461/files/5_PaveDiamondPearlHuggies_Earrings_YG_Stacked_Rich_1bfd1c7e-f302-471f-9a17-eebb160db535.jpg"
-            ],
-            450,
-            ["14k Yellow Gold"],
-            "- Average Carat Weight: 0.089 ct - Diamond Clarity: SI - I - Diamond Color: GHI - Diamond Size: 0.9 mm - Pearl Diameter: 5 mm - 5.5 mm - Outer Diameter: 11 mm - Width: 1.9 mm",
-            "Any time, any place these huggies fit the bill. Handcrafted in 14k solid gold featuring 22 diamonds in a pavé setting and a round freshwater pearl—it’s your essential diamond earring with a fresh twist.",
-            "14k Gold When properly cared for, our 14k gold pieces can last forever. With its beautiful, subtle hue, you can wear your jewelry every day, everywhere."
-        ),
         new Produit("3", "Dôme Huggies", "Earrings",
             ['Sterling Silver'],
             ["https://cdn.shopify.com/s/files/1/0837/7489/8461/files/0_DomeHuggies_earrings_yg_hero_Comp_83a98215-f62f-4b50-8d13-e4ceaade717e.jpg",
@@ -230,29 +190,23 @@ export class ProduitService {
     wishlist: Produit[] = [];
     panierlist: Produit[] = [];
 
-    constructor() { }
+    constructor(private http: HttpClient, public router: Router) { }
 
-    getProducts(): Produit[] {
-        return this.liste_produits
+    getProducts(): Observable<Produit[]> {
+        return this.http.get<Produit[]>("http://localhost:3000/api/products")
     }
 
-    getProductById(id: string): Produit {
-
-        for (let i = 0; i < this.liste_produits.length; i++) {
-            if (this.liste_produits[i]._id == id) {
-                return this.liste_produits[i];
-            }
-        }
-        return this.liste_produits[0]
+    getProductById(id: string): Observable<Produit> {
+        return this.http.get<Produit>("http://localhost:3000/api/product?id=" +id )
     }
 
-    getProductByCategorie(categorie: string): Produit[] {
-        return this.liste_produits.filter((e) => e.categorie == categorie)
+    getProductByCategorie(categorie: string):  Observable<Produit[]> {
+        return this.http.get<Produit[]>("http://localhost:3000/api/product/categorie?categorie="+categorie)         
     }
 
     pushWishList(p: Produit) {
-        if( !this.isDejaInWishList(p))
-        this.wishlist.push(p);
+        if (!this.isDejaInWishList(p))
+            this.wishlist.push(p);
     }
 
     isDejaInWishList(p: Produit): boolean {
