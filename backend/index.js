6e828b4903af24d0945f8e148a55de36dd7a5918
safiylm <121391404+safiylm@users.noreply.github.com>
2024-10-20 20:55:45 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,15 @@
 const express = require('express');
-
+const db = require('./config/db.config.js')
 const app = express();
 
 const port = 3000;
+const router_user = require("./routes/product.route")
 
+app.use(router_user )
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send("hello world");
 });
-
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
