8e1784565bf510047d4a4f340e15cdb348e5d25c
safiylm <121391404+safiylm@users.noreply.github.com>
2024-11-07 11:03:07 +0100

    user account page

--- src/app/user-page/user-page.component.ts
+++ src/app/user-page/user-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { User } from 'src/models/user.model';
+import { UserService } from 'src/services/user-service';
 
 @Component({
   selector: 'app-user-page',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-page.component.scss']
 })
 export class UserPageComponent {
+  user!: User;
+  deconnexion() {
+    localStorage.removeItem("userId")
+    localStorage.setItem('isLoggedIn', "false")
+  }
 
+  constructor(private userService: UserService) { }
+
+  ngOnInit() {
+    this.userService.getUserById(localStorage.getItem("userId") as string ).subscribe({
+      next: (data) => {
+        this.user = data
+      },
+      error: (e) => console.error(e)
+    })
+  }
 }
