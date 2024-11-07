d707bfeb31cc687113cf14c54f0b7aa62ac9d69b
safiylm <121391404+safiylm@users.noreply.github.com>
2024-11-07 11:02:43 +0100

    authentification guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { HomeComponent } from './home/home.component';
 import { CollectionPageComponent } from './collection-page/collection-page.component';
+import { UserPageComponent } from './user-page/user-page.component';
+import { AuthGuard } from './guard/auth.guard';
 
 const routes: Routes = [
   { path: '', component: HomeComponent  },
   { path: 'product/:id', component: ProductPageComponent  },
   { path: 'collections/:categorie', component: CollectionPageComponent  },
+  { path: 'user', component: UserPageComponent,  canActivate: [AuthGuard],  },
 ];
 
 @NgModule({
